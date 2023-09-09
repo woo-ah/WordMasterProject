296c9d80e7d7dda6b99febc7d2778c0d5b022db5
ujiyun <121033437+ujiyun@users.noreply.github.com>
2023-09-09 17:52:30 +0900

    Create & Read 기능 구현

--- src/main/java/org/example/WordCRUD.java
+++ src/main/java/org/example/WordCRUD.java
@@ -26,7 +26,7 @@ public class WordCRUD implements ICRUD{
     public void addWord(){
         Word one = (Word) add();
         list.add(one);
-        System.out.println("새 단어가 단어장에 추가되었습니다 !!!\n");
+        System.out.println("\n새 단어가 단어장에 추가되었습니다 !!!\n");
     }
 
     @Override
