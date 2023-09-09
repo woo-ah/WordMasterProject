f21049e2e746479a04cd89f97b634190161380dd
ujiyun <121033437+ujiyun@users.noreply.github.com>
2023-09-09 17:18:21 +0900

    Create & Read 기능 구현

---
+++ src/main/java/org/example/Word.java
@@ -0,0 +1,59 @@
+package org.example;
+
+public class Word {
+
+
+    private int id;
+    private int level;
+    private String word;
+    private String meaning;
+
+    Word(){}
+    Word(int id, int level, String word, String meaning){
+        this.id = id;
+        this.level = level;
+        this.word = word;
+        this.meaning = meaning;
+    }
+    public int getId() {
+        return id;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public void setMeaning(String meaning) {
+        this.meaning = meaning;
+    }
+
+    @Override
+    public String toString() {
+
+        String slevel = "";
+        for(int i=0; i<level; i++) slevel += "*";
+        String str = String.format("%-3s", slevel)
+                + String.format("%15s", word) + "  " + meaning;
+        return str;
+    }
+}
